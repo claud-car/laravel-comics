9d70314206b5419d408f1658b8ed91ab34bbcacb
claud-car <claudiocarchietti@yahoo.it>
2021-05-12 18:57:41 +0200

    Header completato

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Dc-Comics</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    </head>
+    <body>
+            @include('partials._header')
+
+            @yield('main')
+
+            @include('partials._footer')
+    </body>
+</html>
---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,31 @@
+    <header>
+        <div class="top-header-container">
+            <div class="top-header">
+                <span>DC POWER℠ VISA®</span>
+                <span>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></span>
+            </div>
+        </div>
+        <div class="navbar">
+            <div class="logo">
+                <img src="/img/dc-logo.png" alt="">
+            </div>
+            <div class="list">
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP<i class="fas fa-caret-down"></i></li>
+                </ul>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search"><i class="fas fa-search"></i>
+            </div>
+        </div>
+        <div class="header-jumbo"></div>
+    </header>
